ff0a377d4e6cfba80311fdcb0f1328c4365bba65
Polianskiy Artur <artyr.polianskij@gmail.com>
2024-01-13 02:02:38 +0200

    Updated header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { Separator } from "@app/components/ui/separator";
 import { Fragment } from "react";
 import Link from "next/link";
+import { LogoIcon } from "@app/icons/Logo";
 
 export const Header = () => {
   const headerLinks = [
@@ -19,21 +20,23 @@ export const Header = () => {
     { title: "Скоро в кіно", path: "upcoming" },
   ];
   return (
-    <div className="mt-8">
-      <div className="flex justify-between px-5 sm:px-10 xl:px-40 border-b-neutral-600">
-        <Link href="/">Logo</Link>
+    <div className="mt-2">
+      <div className="flex justify-between px-5 sm:px-10 xl:px-40 border-b-neutral-600 items-center">
+        <Link href="/">
+          <LogoIcon />
+        </Link>
         <div className="flex gap-5">
           {headerLinks.map((link, index) => (
             <Fragment key={link.title}>
               <Link href={`movies/${link.path}`}>{link.title}</Link>
               {index !== headerLinks.length - 1 && (
-                <Separator orientation="vertical" />
+                <Separator className="h-8" orientation="vertical" />
               )}
             </Fragment>
           ))}
         </div>
       </div>
-      <Separator className="mt-8" />
+      <Separator className="mt-2" />
     </div>
   );
 };
